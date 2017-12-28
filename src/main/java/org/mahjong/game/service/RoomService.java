c2c4f32fd3508cc776c803baffbe5b9aa9a99776
YawenYawen <14302010057@fudan.edu.cn>
2017-12-28 22:49:45 +0800

    project setup

--- src/main/java/org/mahjong/game/service/RoomService.java
+++ src/main/java/org/mahjong/game/service/RoomService.java
@@ -356,6 +356,7 @@ public class RoomService {
         result.setMessage("invite success");
         result.setObject(room.getId() + "");
         session.sendMessage(new TextMessage(result.toString()));
+        broadcastRoomPlayers(session);
 
         log.info("用户{}创建了好友房间，session {}，房间id{}，邀请三位好友:{}/{}、{}/{}、{}/{}", user.getName(), session.getId(), room.getId(), friends.get(0).getName(), sessions.get(0).getId(), friends.get(1).getName(), sessions.get(1).getId(), friends.get(2).getName(), sessions.get(2).getId());
         Constants.roomMap.put(room.getId(), room);
